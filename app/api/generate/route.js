a199a7437dd4d26b69492e5922da6fd5c78fa545
raghu693 <sonia.nagar16@gmail.com>
2024-11-10 13:52:02 +0530

    Try 14

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,39 +1,29 @@
 import clientPromise from "@/lib/mongodb";
-import { NextRequest } from "next/server";
 
-export async function POST() {
-  try {
-    let data = await NextRequest.json();
-    let client = await clientPromise;
-    let db = client.db("BitLinks");
-    const collection = db.collection("url");
-    const doc = await collection.findOne({ shortUrl: data.shortUrl });
-    
-    if (doc) {
-      return json({
-        success: false,
-        error: true,
-        message: "Url Already Exists!",
-      });
-    }
+export async function POST(request) {
+  const body = await request.json();
+  const client = await clientPromise;
+  const db = client.db("Bitlinks");
+  const collection = db.collection("url");
 
-    const result = await collection.insertOne({
-      url: data.url,
-      shortUrl: data.shortUrl,
-    });
-
-    return json({
-      success: true,
-      error: false,
-      message: "Url Generated Successfully",
-    });
-  } catch (error) {
-    return json({
+  // Check if the short url exists
+  const doc = await collection.findOne({ shorturl: body.shorturl });
+  if (doc) {
+    return Response.json({
       success: false,
       error: true,
-      message: "An error occurred",
-      details: error.message,
+      message: "URL already exists!",
     });
   }
+
+  const result = await collection.insertOne({
+    url: body.url,
+    shorturl: body.shorturl,
+  });
+
+  return Response.json({
+    success: true,
+    error: false,
+    message: "URL Generated Successfully",
+  });
 }
-// Left At 40:00
